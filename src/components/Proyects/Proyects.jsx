55b3dcc1585ca952c66ef4e1731a1716043f2066
RodriguezSN <gamer.infante51@gmail.com>
2024-10-09 20:07:55 -0300

    estilado apartado proyectos

--- src/components/Proyects/Proyects.jsx
+++ src/components/Proyects/Proyects.jsx
@@ -1,9 +1,23 @@
+import { Link } from "react-router-dom";
+import style from "./Proyects.module.css";
 const Proyects = () => {
 	return (
-		<div>
-			<h1>Mis Proyectos</h1>
-			<button>proyect 1</button>
-			<button>proyect 2</button>
+		<div className={style.ProyectsPadre}>
+			<div className={style.titulo}>
+				<h1>Mis Proyectos</h1>
+			</div>
+			<div className={style.divProyectos}>
+				{/* <button className={style.buttonProyecto}> */}
+				<div className={style.divProyecto}>
+					<Link to="https://pokedex-rodriguezsn.vercel.app/" target="_blank">
+						<img
+							src="https://res.cloudinary.com/dwvdvzg1k/image/upload/v1728513293/phrvdh28jzfwkzje7bgt.png"
+							alt=""
+						/>
+					</Link>
+				</div>
+				{/* </button> */}
+			</div>
 		</div>
 	);
 };
